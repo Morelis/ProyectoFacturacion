da8c1e1d9a26495c245fd09638981350b02f09f7
RashelAlvarez <ralvarez@intelix.biz>
2025-05-23 18:11:17 -0400

    Creacion de clase Modelo Customer

--- src/main/java/com/sisfacturation/demo/customer/models/Customer.java
+++ src/main/java/com/sisfacturation/demo/customer/models/Customer.java
@@ -1,5 +1,112 @@
 package com.sisfacturation.demo.customer.models;
 
-public class Customer {
+import java.io.Serializable;
+import java.sql.Date;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id; 
+
+
+@Entity
+public class Customer implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(nullable = false)
+	private String name;
+
+	private boolean isCustomer;
+	private boolean isActive;
+	private Date created;
+	private Date updated;
+	private String address;
+
+	@Column(nullable = false, unique = true)
+	private String rif;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Customer(String name, boolean isCustomer, boolean isActive, Date created, Date updated, String address,
+			String rif) {
+		super();
+		this.name = name;
+		this.isCustomer = isCustomer;
+		this.isActive = isActive;
+		this.created = created;
+		this.updated = updated;
+		this.address = address;
+		this.rif = rif;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public boolean isCustomer() {
+		return isCustomer;
+	}
+
+	public void setCustomer(boolean isCustomer) {
+		this.isCustomer = isCustomer;
+	}
+
+	public boolean isActive() {
+		return isActive;
+	}
+
+	public void setActive(boolean isActive) {
+		this.isActive = isActive;
+	}
+
+	public Date getCreated() {
+		return created;
+	}
+
+	public void setCreated(Date created) {
+		this.created = created;
+	}
+
+	public Date getUpdated() {
+		return updated;
+	}
+
+	public void setUpdated(Date updated) {
+		this.updated = updated;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getRif() {
+		return rif;
+	}
+
+	public void setRif(String rif) {
+		this.rif = rif;
+	}
 
 }
